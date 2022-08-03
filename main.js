317ce3b7cdbd811dda37384d649efddc72116398
ladyfree <83199278+ladyfree@users.noreply.github.com>
2022-08-04 00:07:11 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 // main.js
 App.onJoinPlayer.Add(function(p) {
-    App.showCenterLabel("Hello Pitta",0x6758FF, 0x000000, 115)
+    App.showCenterLabel("Hello Pitta");
+    //App.showCenterLabel("Hello Pitta",0x6758FF, 0x000000, 115)// text, 전경색, 배경색, offset
 });
